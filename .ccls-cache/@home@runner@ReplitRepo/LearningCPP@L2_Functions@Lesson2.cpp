423fab9d6d1500179d1999d7ff12b5656bbb07c5
JasonColtier <jasoncoltier@gmail.com>
2023-04-22 17:48:15 +0000

    data types

---
+++ .ccls-cache/@home@runner@ReplitRepo/LearningCPP@L2_Functions@Lesson2.cpp
@@ -0,0 +1,18 @@
+#include "iostream"
+
+using namespace std;
+
+void MyFunction(){
+  cout<<"function call"<<endl;
+}
+
+//lesson about functions
+int main(){
+
+  cout<<"lesson 2"<<endl;
+
+  MyFunction();
+  
+  return 0;
+}
+
---
+++ .ccls-cache/@home@runner@ReplitRepo/LearningCPP@Lesson2.cpp
@@ -0,0 +1,18 @@
+#include "iostream"
+
+using namespace std;
+
+void MyFunction(){
+  
+}
+
+//lesson about functions
+int main(){
+
+  cout<<"lesson 2"<<endl;
+
+  MyFunction();
+  
+  return 0;
+}
+
